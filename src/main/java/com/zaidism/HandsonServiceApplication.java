dbea4f388b2e3eac7e5bffc80b97a2da3c5416a6
git-zaidism <zaidism7@gmail.com>
2024-08-28 16:36:58 +0530

    "initial spring boot commit"

---
+++ src/main/java/com/zaidism/HandsonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zaidism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsonServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zaidism/HandsonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaidism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
